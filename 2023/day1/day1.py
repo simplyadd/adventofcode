65f22756b8125960afe8b03fc746cfed33fe1438
Ice <gpreciousalyssa@gmail.com>
2023-11-30 23:55:19 -0600

    Add pseudo code for day 1

---
+++ 2023/day1/day1.py
@@ -0,0 +1,16 @@
+import sys
+
+def main():
+    calVals = []
+    with open("input.txt") as fp:
+        while True:
+            line = fp.readline()
+            #get first char
+            #get last char
+            #concat chars
+            #save to list
+            #sum all vals
+    fp.close()
+
+if __name__ == "__main__":
+    main()
