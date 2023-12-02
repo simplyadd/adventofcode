82f5c55f657ea897dfc582c91b36ee35d6f123b0
Ice <gpreciousalyssa@gmail.com>
2023-12-02 02:40:25 -0600

    Solve Part 2 of puzzle by using regex lookahead

--- 2023/day1/day1.py
+++ 2023/day1/day1.py
@@ -1,33 +1,29 @@
-def getDigit(line: str) -> str:
-    for c in line:
-        if c.isnumeric() == True:
-            return c
-
+import re
 
 def main():
     firstDigit, lastDigit, sum = 0, 0, 0
-    d = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5,
-         'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
-
-    file1 = open('test.txt', 'r')
+    
+    file1 = open('input.txt', 'r')
     while True:
         line = file1.readline()
-
+        
         if not line:
             break
-        '''
-        s = [key for key in d.keys() if(key in line)]
-        print(s)
-        '''
-        for k, v in d.items():
-            if k in line:
-                line = line.replace(k, str(v))
-
         
-        firstDigit = getDigit(line)
-        lastDigit = getDigit(reversed(line))
+        x = re.findall('(?=([1-9]|one|two|three|four|five|six|seven|eight|nine))', line)
         
-        calVal = int(firstDigit + lastDigit)
+        d = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5,
+            'six': 6, 'seven': 7, 'eight': 8, 'nine': 9,}
+        
+        firstDigit = x[0]
+        lastDigit = x[-1]
+        
+        if firstDigit in d.keys():
+            firstDigit = d[firstDigit]
+        if lastDigit in d.keys():
+            lastDigit = d[lastDigit]
+        
+        calVal = int(str(firstDigit) + str(lastDigit))
 
         sum += calVal
     print(sum)
