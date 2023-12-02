1d65dc2a8087bbb42a39938dd7748e0bda0ea574
Ice <gpreciousalyssa@gmail.com>
2023-12-01 23:12:27 -0600

    Partially solve Part 2; 'eightwothree' -> 'eigh23'

--- 2023/day1/day1.py
+++ 2023/day1/day1.py
@@ -6,13 +6,23 @@ def getDigit(line: str) -> str:
 
 def main():
     firstDigit, lastDigit, sum = 0, 0, 0
+    d = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5,
+         'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
 
-    file1 = open('input.txt', 'r')
+    file1 = open('test.txt', 'r')
     while True:
         line = file1.readline()
 
         if not line:
             break
+        '''
+        s = [key for key in d.keys() if(key in line)]
+        print(s)
+        '''
+        for k, v in d.items():
+            if k in line:
+                line = line.replace(k, str(v))
+
         
         firstDigit = getDigit(line)
         lastDigit = getDigit(reversed(line))
