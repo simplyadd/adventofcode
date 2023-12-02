764672014853d87b55382c8371cb5e2ec5bdc0b2
Ice <gpreciousalyssa@gmail.com>
2023-12-01 21:30:14 -0600

    Solve Part 1 of puzzle

--- 2023/day1/day1.py
+++ 2023/day1/day1.py
@@ -1,16 +1,37 @@
-import sys
+# Day 1
+# Get sum of calibration values in each line.
+# The calibration value can be found by combining the first and last 
+# digit (in that order) to form a single two-digit number.
 
 def main():
     calVals = []
-    with open("input.txt") as fp:
-        while True:
-            line = fp.readline()
-            #get first char
-            #get last char
-            #concat chars
-            #save to list
-            #sum all vals
-    fp.close()
+    firstDigit, lastDigit, sum = 0, 0, 0
+
+    file1 = open('input.txt', 'r')
+
+    while True:
+        line = file1.readline()
+
+        if not line:
+            break
+    
+        for i in line:
+            if i.isnumeric() == True:
+                firstDigit = str(i)
+                break
+        for j in reversed(line):
+            if j.isnumeric() == True:
+                lastDigit = str(j)
+                break
+        #concat chars
+        num = int(firstDigit + lastDigit)
+
+        sum += num
+        calVals.append(num)
+
+    print(sum)
+    file1.close()
 
 if __name__ == "__main__":
     main()
+    
