93cc1aa0232212688c696b9e84c7787dd6670f70
Ice <gpreciousalyssa@gmail.com>
2023-12-05 00:05:52 -0600

    Delete multiplication by 1

--- 2023/day4/d4p2.py
+++ 2023/day4/d4p2.py
@@ -5,7 +5,7 @@ def main():
   try:
     input = open(sys.argv[1], 'r')
   except:
-    input = open('test1.txt', 'r')
+    input = open('input.txt', 'r')
 
   d = {}
   sum, cardN = 0, 0
@@ -26,7 +26,7 @@ def main():
           matchCnt += 1
 
     for i in range(matchCnt):
-      d[cardN+i+1] = d.get(cardN+i+1, 0) + 1 * d[cardN]
+      d[cardN+i+1] = d.get(cardN+i+1, 0) + d[cardN]
     
   for v in d.values():
     sum += v
