6d462b7ffdad2a1ce4c2e4e021aacfc9f7ca6e09
Ice <gpreciousalyssa@gmail.com>
2023-12-11 21:36:00 -0600

    Solve Day 8 Part 1 of puzzle

---
+++ 2023/day8/d8p1.py
@@ -0,0 +1,72 @@
+import re
+import sys
+
+line1 = ''
+nodes = []
+stepCnt, cnt = 0, 0
+
+class Node:
+  __slots__ = ('value', 'left', 'right')
+  def __init__(self, value: str, left: str, right: str):
+    self.value = value
+    self.left = left
+    self.right = right
+
+
+def searchNode(step: str) -> str:
+  global nodes, stepCnt, cnt
+  for node in nodes:
+    if step == 'ZZZ':
+        return step
+    if step == node.value:
+      if cnt == len(line1)-1:
+        cnt = 0
+      
+      dest = line1[cnt]
+      if dest == 'L':
+        step = node.left
+      if dest == 'R':
+        step = node.right
+
+      #print(stepCnt, step, dest)
+      cnt += 1
+      stepCnt += 1
+      return step
+
+def main():
+  global nodes, stepCnt, cnt, line1
+  nodesVal = []
+  step = 'AAA'
+  
+  try:
+    input = open(sys.argv[1], 'r')
+  except:
+    input = open('input.txt', 'r')
+
+  line1 = input.readline()
+
+  for line in input:
+    try:
+      value, left, right = re.findall(r'[A-Z]+', line)
+    except:
+      continue
+    
+    nodes.append(Node(value, left, right))
+    nodesVal.append(value)
+
+    if step in nodesVal:
+      step = searchNode(step)
+    elif step == 'ZZZ':
+      break
+    else:
+      continue
+  input.close()
+
+  while (step != 'ZZZ'):
+    step = searchNode(step)
+
+  print(stepCnt)
+  #7766929, 528.324
+if __name__ == "__main__":
+  main()
+    
