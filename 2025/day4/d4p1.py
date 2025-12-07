b95ccdb9edc3200cb5ce23afcaca9107e2b79a3b
simplyadd <69213787+simplyadd@users.noreply.github.com>
2025-12-06 18:33:39 -0600

    2025 d4p1: Fix edge test case (test1.txt)

--- 2025/day4/d4p1.py
+++ 2025/day4/d4p1.py
@@ -1,23 +1,21 @@
 import re
 import sys
+sys.path.append('../../')
+from parser import *
 
-threeLines = ['', '', '']
+threeLines = ['', '']
 total = 0
-
 
 def check_if_accessible(start: int) -> bool:
   global threeLines
-  try:
-    adjacent = 0
-    adjacent += count_adjacent(threeLines[0], start)
-    adjacent += count_adjacent(threeLines[1], start)
-    adjacent += count_adjacent(threeLines[2], start)
-  except: # For last line
-    adjacent = 0
-    adjacent += count_adjacent(threeLines[0], start)
-    adjacent += count_adjacent(threeLines[1], start)
-  return True if (adjacent-1 < 4) else False
+  adjacent = -1
 
+  for i in threeLines:
+    try:    adjacent += count_adjacent(i, start)
+    except: break
+
+    if (adjacent >= 4): return False
+  return True
 
 def count_adjacent(line: str, start: int) -> int:
   adj = 0
@@ -25,7 +23,7 @@ def count_adjacent(line: str, start: int) -> int:
   max = start+2 if (start < line_len-1) else line_len
   for i in line[min:max]:
     if i != '.':
-        adj += 1
+      adj += 1
   return adj
 
 
@@ -34,61 +32,51 @@ def find_rolls(line: str):
   for roll in re.finditer(r'@', line):
     index = roll.start() # Returns index of matched str
     found = check_if_accessible(index)
-    total += 1 if (found) else 0
+    if (found): total += 1
   return
-
 
 def find_rolls_dbg(line: str):
   global total
   test  = ''
   for i, c in enumerate(line):
-    if (c == '@'):
-      found = check_if_accessible(i)
-      total += 1 if (found) else 0
-    else:
-      found = False
-    test += 'x' if (found) else line[i]
-  print(test)
-  return
+    if (c == '@'): found = check_if_accessible(i)
+    else:          found = False
 
+    if (found):
+      total += 1
+      test += 'x'
+    else: test += line[i]
+  logger.debug(test)
+  return
 
 def main():
   global threeLines
   global total
 
-  try:
-    input = open(sys.argv[1], 'r')
-  except:
-    input = open('input.txt', 'r')
+  logger.debug('----------DEBUG MODE START----------')
+  input = open(args.file, 'r')
 
-  if DEBUG_MODE:
-    print('----------DEBUG MODE START----------\n')
+  line1 = input.readline()
+  threeLines.append(line1.rstrip())
 
   for line in input:
     threeLines.append(line.rstrip()) # Add new index
     threeLines.pop(0)                # Keep length at 3
-    curr = threeLines[1]
 
     # Iterates through the whole string when debugging
-    find_rolls_dbg(curr) if DEBUG_MODE else find_rolls(curr)
+    find_rolls_dbg(threeLines[1]) if (log_level==logging.DEBUG) else find_rolls(threeLines[1])
 
   # For last line
   threeLines.pop(0)
-  find_rolls_dbg(curr) if DEBUG_MODE else find_rolls(curr)
+  find_rolls_dbg(threeLines[1]) if (log_level==logging.DEBUG) else find_rolls(threeLines[1])
 
   input.close()
-  if DEBUG_MODE:
-    print('----------DEBUG MODE END------------\n')
-  print(total)
+  logger.debug('----------DEBUG MODE END------------')
+  logger.info(total)
 
 if __name__ == "__main__":
-  DEBUG_MODE = 1 if (sys.argv[-1]=='DEBUG') else 0
-
   # Get length of first line before starting
-  try:
-    input = open(sys.argv[1], 'r')
-  except:
-    input = open('input.txt', 'r')
+  input = open(args.file, 'r')
   line_len = len(input.readline().rstrip())
   input.close()
   main()
