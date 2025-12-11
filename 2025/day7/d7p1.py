54aa2fcd1ddd4c9ab043f94fc939a9795e676862
simplyadd <69213787+simplyadd@users.noreply.github.com>
2025-12-10 22:13:31 -0600

    2025 d7p1: add initial (answer is too high for input.txt)

---
+++ 2025/day7/d7p1.py
@@ -0,0 +1,42 @@
+import re
+import sys
+sys.path.append('../../')
+from parser import *
+
+def main():
+  num_last_add = 1  # 'S' starts with connecting with 1 line
+  stack = []
+  temps = []
+  total = -1        # First splitter does not count
+
+  f = open(args.file, 'r')
+  l = f.readline()
+  stack.append(l.index('S'))
+
+  for l in f:
+    logger.debug(l)
+    line = list(l.strip())
+
+    i = 0
+    elem = stack[i]
+    while (stack):
+      if (line[elem] == '^'):
+        temps.append(elem-1)
+        temps.append(elem+1)
+        stack.pop(i)
+        total += 1
+        i -= 1
+      i += 1
+      if (i < len(stack)):
+        elem = stack[i]
+      else:
+        break
+
+    if (temps):
+      intermediate = list(set(temps))
+      stack.extend(intermediate)
+    temps = []
+  logger.info(total)
+
+if __name__ == "__main__":
+  main()
