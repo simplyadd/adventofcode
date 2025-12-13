d102ba2b46479cf83af134a86427ccec276ea0c8
simplyadd <69213787+simplyadd@users.noreply.github.com>
2025-12-12 20:46:06 -0600

    2025 d7p1: remove unnecessary lines

--- 2025/day7/d7p1.py
+++ 2025/day7/d7p1.py
@@ -11,28 +11,19 @@ def main():
   l = f.readline()
   stack.append(l.index('S'))
 
-  current_splits = [0] * len(l.rstrip())
-  line_splits = current_splits.copy()
-
   for l in f:
     line = list(l.strip())
 
     i = 0
     elem = stack[i]
 
-    logger.debug(f'{''.join(str(i) for i in current_splits)} \t{total}')
     while (stack):
       if (line[elem] == '^'):
-        left, right = elem-1, elem+1
-        if ((current_splits[left] == 0) or (current_splits[right] == 0)):
-          line_splits[left]  = 1
-          line_splits[elem]  = 0
-          line_splits[right] = 1
-          temps.append(left)
-          temps.append(right)
-          stack.pop(i)
-          total += 1
-          i -= 1
+        temps.append(elem-1)
+        temps.append(elem+1)
+        stack.pop(i)
+        total += 1
+        i -= 1
       i += 1
       if (i < len(stack)):
         elem = stack[i]
@@ -42,8 +33,6 @@ def main():
     if (temps):
       stack.extend(temps)
       stack = list(set(stack))
-
-    line_splits = current_splits.copy()
     temps = []
   logger.info(total)
 
